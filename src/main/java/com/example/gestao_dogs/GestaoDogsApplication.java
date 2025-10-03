62cfc60ed9625047b231e21d8a6eb2008ecca9ff
devlucascalado <devlucascalado@gmail.com>
2025-10-02 21:17:17 -0300

    projeto

---
+++ src/main/java/com/example/gestao_dogs/GestaoDogsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestao_dogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestao_dogs/GestaoDogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestao_dogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
