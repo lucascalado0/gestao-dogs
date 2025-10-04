a6a7b0b89b56260d32f2959c9f330fb8ce51a354
devlucascalado <devlucascalado@gmail.com>
2025-10-03 21:26:04 -0300

    service-class

---
+++ src/main/java/com/example/gestao_dogs/model/Service.java
@@ -0,0 +1,42 @@
+package com.example.gestao_dogs.model;
+
+import com.example.gestao_dogs.model.enums.ServiceType;
+import com.example.gestao_dogs.model.enums.StatusPayment;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity(name = "Service")
+@Table(name = "tb_services")
+public class Service {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "service_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "service_type", nullable = false)
+    private ServiceType serviceType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_payment", nullable = false)
+    private StatusPayment statusPayment;
+
+    @Column(name = "initial_date", nullable = false)
+    private LocalDate initialDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+    @Column(name = "service_value", nullable = false)
+    private Double value;
+
+    @ManyToMany
+    @JoinTable(name = "tb_service_pets",
+            joinColumns = @JoinColumn(name = "service_id"),
+            inverseJoinColumns = @JoinColumn(name = "pet_id"))
+    private List<Pets> pets;
+}
