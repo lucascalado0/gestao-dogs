2bdab001d357c5ad927b49fbbf9f7e0726783bca
devlucascalado <devlucascalado@gmail.com>
2025-10-03 21:26:41 -0300

    pets-repository

---
+++ src/main/java/com/example/gestao_dogs/repository/PetsRepository.java
@@ -0,0 +1,18 @@
+package com.example.gestao_dogs.repository;
+
+import com.example.gestao_dogs.model.Guardian;
+import com.example.gestao_dogs.model.Pets;
+import com.example.gestao_dogs.model.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface PetsRepository extends JpaRepository<Pets, Long> {
+    List<Pets>findByGuardian(Guardian guardian);
+    List<Pets>findByContainingNameIgnoreCase(String name);
+    List<Pets>findByBreedIgnoreCase(String breed);
+    List<Pets>findByServicesContaining(Service service);
+}
