0be962bf5f9d63af572b67be0d1b346cab1ccb8d
devlucascalado <devlucascalado@gmail.com>
2025-10-03 21:25:49 -0300

    guardianAdress-class

---
+++ src/main/java/com/example/gestao_dogs/model/GuardianAdress.java
@@ -0,0 +1,28 @@
+package com.example.gestao_dogs.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Data
+@Embeddable
+public class GuardianAdress {
+
+    @Column(name = "street", nullable = false, length = 150)
+    private String street;
+
+    @Column(name = "city", nullable = false, length = 100)
+    private String city;
+
+    @Column(name = "state", nullable = false, length = 100)
+    private String state;
+
+    @Column(name = "cep", nullable = false, length = 20)
+    private String zipCode;
+
+    @Column(name = "number", nullable = false, length = 5)
+    private String number;
+
+    @Column(name = "complement", length = 150)
+    private String complement;
+}
