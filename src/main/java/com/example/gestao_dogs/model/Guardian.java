2aaef0dbaeddf1a1d588762d2dc3057d61ebd01b
devlucascalado <devlucascalado@gmail.com>
2025-10-03 21:25:27 -0300

    guardian-class

---
+++ src/main/java/com/example/gestao_dogs/model/Guardian.java
@@ -0,0 +1,12 @@
+package com.example.gestao_dogs.model;
+
+import java.util.List;
+
+public class Tutor {
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private TutorAdress endereco;
+    private List<Pets> pets;
+}
