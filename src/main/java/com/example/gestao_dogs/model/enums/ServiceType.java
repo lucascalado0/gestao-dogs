6383585729d7fb85c0a9fca1373fa7284b1fc795
devlucascalado <devlucascalado@gmail.com>
2025-10-06 14:58:00 -0300

    service-type-enum

--- src/main/java/com/example/gestao_dogs/model/enums/ServiceType.java
+++ src/main/java/com/example/gestao_dogs/model/enums/ServiceType.java
@@ -1,7 +1,16 @@
 package com.example.gestao_dogs.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum ServiceType {
-    DOG_WALK,
-    PET_SITTING,
-    HOSTING
+    DOG_WALK("Passeio"),
+    PET_SITTING("Visita e cuidado em domicílio"),
+    HOSTING("Hospedagem");
+
+    private final String description;
+
+    ServiceType(String description) {
+        this.description = description;
+    }
 }
