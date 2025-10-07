4d1cffa6f2e1601c1caa7c9d4eb701c1da8fc914
devlucascalado <devlucascalado@gmail.com>
2025-10-06 23:09:35 -0300

    hexception-handler

--- src/main/java/com/example/gestao_dogs/infra/RestExceptionHandler.java
+++ src/main/java/com/example/gestao_dogs/infra/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.gestao_dogs.infra;
 
+import com.example.gestao_dogs.exception.guardian_exception.GuardianArgumentException;
 import com.example.gestao_dogs.exception.guardian_exception.GuardianNotFoundException;
 import com.example.gestao_dogs.exception.pets_exception.PetNotFoundException;
 import com.example.gestao_dogs.exception.service_exception.ServiceArgumentException;
@@ -14,32 +15,32 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(GuardianNotFoundException.class)
-    private ResponseEntity<String> GuardianNotFoundHandler(GuardianNotFoundException exception){
+    private ResponseEntity<RestErrorMessage> GuardianNotFoundHandler(GuardianNotFoundException exception){
         RestErrorMessage restErrorMessage = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Guardian not found.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(restErrorMessage);
     };
 
-    @ExceptionHandler(GuardianNotFoundException.class)
-    private ResponseEntity<String> GuardianArgumentHandler(GuardianNotFoundException exception){
+    @ExceptionHandler(GuardianArgumentException.class)
+    private ResponseEntity<RestErrorMessage> GuardianArgumentHandler(GuardianNotFoundException exception){
         RestErrorMessage restErrorMessage = new RestErrorMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Guardian argument error.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(restErrorMessage);
     };
 
     @ExceptionHandler(PetNotFoundException.class)
-    private ResponseEntity<String> PetNotFoundHandler(PetNotFoundException exception){
+    private ResponseEntity<RestErrorMessage> PetNotFoundHandler(PetNotFoundException exception){
         RestErrorMessage restErrorMessage = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pet not found.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(restErrorMessage);
     };
 
     @ExceptionHandler(ServiceNotFoundException.class)
-    private ResponseEntity<String> ServiceNotFoundHandler(ServiceNotFoundException exception){
+    private ResponseEntity<RestErrorMessage> ServiceNotFoundHandler(ServiceNotFoundException exception){
         RestErrorMessage restErrorMessage = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Service not found.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(restErrorMessage);
     };
 
     @ExceptionHandler(ServiceArgumentException.class)
-    private ResponseEntity<String> ServiceArgumentHandler(ServiceArgumentException exception){
+    private ResponseEntity<RestErrorMessage> ServiceArgumentHandler(ServiceArgumentException exception){
         RestErrorMessage restErrorMessage = new RestErrorMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Service argument error.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(restErrorMessage);
     };
 }
