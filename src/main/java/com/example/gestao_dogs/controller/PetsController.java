23bdc00a4d9d84d26eb1268635ccfc729a4405ea
devlucascalado <devlucascalado@gmail.com>
2025-10-06 23:09:24 -0300

    pets-controller

--- src/main/java/com/example/gestao_dogs/controller/PetsController.java
+++ src/main/java/com/example/gestao_dogs/controller/PetsController.java
@@ -1,4 +1,10 @@
 package com.example.gestao_dogs.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pets")
 public class PetsController {
 }
