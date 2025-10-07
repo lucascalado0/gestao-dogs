352c9095f4f99353061eee77550987232f110a8f
devlucascalado <devlucascalado@gmail.com>
2025-10-06 23:08:32 -0300

    guardian-not-found

--- src/main/java/com/example/gestao_dogs/exception/guardian_exception/GuardianNotFoundException.java
+++ src/main/java/com/example/gestao_dogs/exception/guardian_exception/GuardianNotFoundException.java
@@ -2,6 +2,10 @@ package com.example.gestao_dogs.exception.guardian_exception;
 
 public class GuardianNotFoundException extends RuntimeException {
     public GuardianNotFoundException(String message) {
-        super("Guardian Not Found: " + message);
+        super(message);
+    }
+
+    public GuardianNotFoundException(){
+        super("Guardian not found");
     }
 }
