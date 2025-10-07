7b357c1d69ff5da03448f465c778656258a317b7
devlucascalado <devlucascalado@gmail.com>
2025-10-06 23:08:52 -0300

    guardian-argument

--- src/main/java/com/example/gestao_dogs/exception/guardian_exception/GuardianArgumentException.java
+++ src/main/java/com/example/gestao_dogs/exception/guardian_exception/GuardianArgumentException.java
@@ -2,6 +2,10 @@ package com.example.gestao_dogs.exception.guardian_exception;
 
 public class GuardianArgumentException extends RuntimeException {
     public GuardianArgumentException(String message) {
-        super("Guardian Argument Error: " + message);
+        super(message);
+    }
+
+    public GuardianArgumentException(){
+        super("Invalid argument for Guardian");
     }
 }
